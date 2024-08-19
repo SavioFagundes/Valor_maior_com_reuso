3294e1e005229511b71ef04eb039e7dd07f0f696
Savio Fagundes <akirasfm@gmail.com>
2024-08-19 20:18:17 -0300

    Adicionar arquivos de projeto.

---
+++ Valor_maior_com_reuso/PrintService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Valor_maior_com_reuso
+{
+    internal class PrintService<T>
+    {
+        private T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidCastException("PrintService if full");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+        public object First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidCastException("PrintService if full");
+            }
+            return _values[0];
+        }
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.Write("]");
+        }
+    }
+}
---
+++ Valor_maior_com_reuso/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Valor_maior_com_reuso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<string> printservice = new PrintService<string>();
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                string x = Console.ReadLine();
+                printservice.AddValue(x);
+            }
+            int a = (int)printservice.First();
+            int b = a + 2;  
+
+            printservice.Print();
+            Console.WriteLine();
+            Console.WriteLine("Firt: " + printservice.First());
+        }
+    }
+}
